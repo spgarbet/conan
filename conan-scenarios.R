790634ea15da8595e9ff53e1fee484f4a432cab2
Shawn Garbett <shawn@garbett.org>
2022-04-20 14:07:49 -0500

    Refactored, edge cases working

--- conan-scenarios.R
+++ conan-scenarios.R
@@ -1,4 +1,29 @@
+
 x <- read.csv("conan-scenarios.csv", stringsAsFactors=FALSE, skip=1)
+
+
+# x <- data.frame(
+#   Scenario = c("Zero", "Zero.1", "One", "Two", "Three", "Four"),
+#   Players  = NA,
+#   Book     = NA,
+#   Mini0    = c(NA, NA, NA, NA, NA, NA), # Not used
+#   Mini0.1  = c(NA, NA, NA, NA, NA, NA),
+#   Mini1    = c(NA, NA,  1,  1,  1,  1),
+#   Mini2    = c(NA, NA, NA,  1,  1,  1),
+#   Mini2.1  = c(NA, NA, NA,  1,  1,  1),
+#   Mini3    = c(NA, NA, NA, NA,  1,  1)
+# )
+
+# x <- data.frame(
+#   Scenario = c("Single", "Double.1", "Double.2"),
+#   Players  = NA,
+#   Book     = NA,
+#   Mini1    = c( 1, NA, NA),
+#   Mini2    = c( 1, NA, NA),
+#   Mini3    = c(NA,  1,  1),
+#   Mini4    = c(NA,  1,  1)
+# )
+
 
 x <- subset(x, !is.na(Scenario) & Scenario != "")
 x <- x[, !names(x) %in% c("X", "X.1", "X.2", "X.3", "X.4", "X.5", "X.6")]
@@ -10,49 +35,88 @@ rownames(y) <- x$Scenario
 # Turn into 0 or 1 groups
 y[] <- vapply(y, function(z) if(is.na(z)) 0 else 1, numeric(1))
 
+
+
+
+
 # Now Find Minimums!
 
-count <- 1
-doit <- function(y)
+paint_order <- function(y)
 {
-  while(length(rownames(y)) > 1)
+  paint <- list()
+  pos   <- 1
+  
+  while(length(rownames(y)) > 0)
   {
-    min_paint <- min(rowSums(y))
-    possibilities <- rownames(y)[rowSums(y) == min_paint]
-    previous <- -1
-    chosen   <- NULL
-    eliminate <- c()
-    for(i in possibilities)
+    min_paint     <- min(rowSums(y)) # Minimum groups to paint
+    possibilities <- rownames(y)[rowSums(y) == min_paint] # Possible scenarios
+    
+    if(sum(y) == 0) # Remaining Unused
     {
-      paints <- y[rownames(y) == i,]
-      paints <- paints[paints > 0]
-  
-      touches <- if(length(names(paints)) == 1)
+      paint[[pos]] = list(colnames(y), NA)
+      y <- NULL
+    } else 
+    if(min_paint == 0) # Nothing to paint
+    {
+      paint[[pos]] = list(NA, possibilities)
+      
+      y <- y[!rownames(y) %in% possibilities, ,drop=FALSE]
+    } else # find the best set to paint
+    {
+      previous  <- -1   # Number required to paint
+      chosen    <- NULL # Chosen at this number
+      eliminate <- c()  # Set to Paint
+      
+      for (i in possibilities) # Consider each scenario
       {
-        sum(y[,names(paints)] > 0)
-      } else
-      { 
-        sum(rowSums(y[,names(paints)]) > 0)
+        paints  <- y[i, ]
+        if(length(paints)==1) names(paints) <- colnames(y) # Don't know why this is necessary, stupid R
+        opens   <- apply(y,1,function(x) all(x==paints))
+        touches <- sum(y[, paints==1, drop=FALSE])
+      
+        if (touches > previous)
+        {
+          previous  <- touches
+          chosen    <- names(opens[opens])
+          eliminate <- names(paints)[paints > 0]
+        }
       }
-      if(touches > previous)
-      {
-        previous <- touches
-        chosen <- i
-        eliminate <- paints
-      }
+
+      paint[[pos]] = list(eliminate, chosen)
+      
+      rows <- rownames(y) %in% chosen
+      cols <- colnames(y) %in% eliminate
+      y <- y[!rows, !cols, drop=FALSE]
     }
+    pos <- pos + 1
+
+  }
+  # Any minis that have no scenarios
+  if(length(colnames(y)) > 0) paint[[pos]] <- list(colnames(y), NA)
   
-    cat(count, " '", chosen, "' by painting: ", paste(names(eliminate), collapse=", "), "\n", sep="")
-    count <- count + 1
-    thelast <- rownames(y)
-    rows <- !rownames(y) %in% chosen
-    cols <- !colnames(y) %in% names(eliminate)
-    if(sum(rows) > 0 && sum(cols) > 0) y <- y[rows, cols, drop=FALSE]
-    if(length(rownames(y)) == 0) cat(paste0("'", thelast, "'"))
-    if(length(rownames(y)) == 1) cat(count, " '", rownames(y), "' by painting: ", paste(colnames(y), collapse=", "), "\n", sep="")
+  paint
+}
+
+print_order <- function(z)
+{
+  count     <- 0
+  for(i in 1:length(z))
+  {
+    minis     <- z[[i]][[1]]
+    scenarios <- z[[i]][[2]]
+
+    
+    if(length(minis) == 1     && is.na(minis))     minis     <- "Paint Nothing"
+    if(length(scenarios) == 1 && is.na(scenarios)) scenarios <- "Not Used In Any Scenario"
+    
+    cat("-", paste(minis, collapse=", "), "\n")
+    cat(paste("    ", (1:length(scenarios))+count, rep(". ", length(scenarios)), scenarios, sep="", collapse="\n"), "\n")
+    
+    count <- count + length(scenarios)
   }
 }
-#doit(y)
+
+#print_order(paint_order(y))
 
 
 # 2nd Run with what I randomly painted already eliminated
@@ -72,7 +136,7 @@ y <- y[, !colnames(y) %in% c(
   "Thaug", "Tentacles",
   "Pirates",
   "Dark.Demon", "Giant.Snake",
-  "Thak", 
+  "Thak",
   "Pallantides", "Olgerd.Vladislav", "Yogah.of.Yag",
   "Thog",
   "Bossonian.Guards",
@@ -88,47 +152,46 @@ y <- y[, !colnames(y) %in% c(
   ,"Giant.Wolves"
   ,"Giant.Spider" ,"Giant.Scorpion","Hyperborean.Primitive"
   ,"Zelata" ,"Belit..Savage." ,"Balthus.Slasher" ,"Ageera" ,"Belit..Brom." ,"Belit..Xavier.","Akivasha"
-  ,"Mummies" 
+  ,"Mummies"
   ,"Skeletons", "Crows", "Bone.Golem", "Swamp.Demon", "Earth.Demon", "Forest.Demon", "Grey.Man.Ape"
   ,"Pelias", "Warlock", "Natohk", "Khemsa"
   ,"Scorpion.Broodmother"
   ,"Atali", "Niord", "Kerim.Shah", "Kothian.Archer"
   ,"Gitara", "Valkyrie", "Winged.One"
-  # 104 Scenarios opened 
-  
-  ,"Crossbowmen" 
+  # 104 Scenarios opened
+
+  #,"Crossbowmen"
   # 106 Scenarios opened (+1) (10 models)
-  
+
   # Remaining critters, into Nordheim (3 models)
-  ,"Giants" ,"Sabertooth.Tiger"
+  #,"Giants" ,"Sabertooth.Tiger"
   # 109 Scenarios opened (+3)
-  
-    
-  ,"Kushite.Witch.Hunters"  
+
+  #,"Kushite.Witch.Hunters"
   # 111 Scenarios opened (+2)  (4 models)
 
   # Armoured (King Complete)
-  ,"Black.Dragons"
+  #,"Black.Dragons"
   # 112 Scenarios opened (+1)  (5 Models)
-  
-  , "Dragon"
+
+  #, "Dragon"
   # 113 (+1) (1 Model)
-  
-  ,"Black.Ones"
+
+  #,"Black.Ones"
   # 114 Scenarios opened (+1)  (10 models)
 
   # Stygia leaders
-  ,"Ikhmet" ,"Shentu" ,"Thoth.Amon"
+  #,"Ikhmet" ,"Shentu" ,"Thoth.Amon"
   # 115 Scenarios opened (+1) (3 models)
-  
+
   # Stygian complete (10 models)
   #,"Eternal.Guard","Assassins"
   # 121 Scenarios opened (+5)
-  
+
   # Finish Nordheim (15 models)
   #,"Aesir.Warriors" ,"Vanir.Warriors"
   # 126 Scenarios opened (+5)
-  
+
   # Khitai (22 models)
   #,"Shuang.Mian" ,"Javelin.Throwers"
   #,"Honor.Guards" ,"Khitan.Guards"
@@ -137,6 +200,5 @@ y <- y[, !colnames(y) %in% c(
 )]
 
 cat("\n\nFrom What I have painted!\n")
-doit(y)
 
-  
+print_order(paint_order(y))
